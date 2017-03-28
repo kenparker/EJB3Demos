67ec52e8c7f77762ce09321bad8c11aefaa3ed00
angelo <magangde@yahoo.de>
2017-03-28 10:37:14 +0200

    enhanced Version

--- ejb-in-ear/ejb/src/main/java/com/maggioni/InfoCreator.java
+++ ejb-in-ear/ejb/src/main/java/com/maggioni/InfoCreator.java
@@ -1,15 +1,32 @@
 package com.maggioni;
 
 import java.util.logging.Logger;
+import javax.ejb.Remove;
 import javax.ejb.Stateful;
 
 @Stateful 
 public class InfoCreator{
 
     private static final Logger log = Logger.getLogger("root.InfoCreator");
-    public String sayHello() {
-      
-       log.info("say Hello called");
-       return "Hello there";
+    
+    private String firstName;
+    private String lastName;
+    
+    private String name;
+        
+    public void setFirstName(String firstName) {
+        log.info("-->> first name : " +  firstName);
+        this.firstName = firstName;
+    }
+    
+    public void setLastName(String lastName) {
+        log.info("-->> last name : " +  lastName);
+        this.lastName = lastName;
+    }
+    
+    @Remove
+    public String createAccount() {
+        this.name = firstName+lastName;
+        return name;
     }
 }
--- ejb-in-ear/web/src/main/java/com/maggioni/Info.java
+++ ejb-in-ear/web/src/main/java/com/maggioni/Info.java
@@ -2,6 +2,7 @@ package com.maggioni;
 
 import com.maggioni.InfoCreator;
 import java.io.Serializable;
+import java.util.logging.Logger;
 import javax.ejb.EJB;
 import javax.inject.Named;
 import javax.enterprise.context.SessionScoped;
@@ -10,13 +11,25 @@ import javax.enterprise.context.SessionScoped;
 @SessionScoped
 public class Info implements Serializable{
 
+    private static final Logger log = Logger.getLogger("root.InfoCreator");
     private static final long serialVersionUID = 1L;
     
     @EJB
     InfoCreator infoCreator;
    
     
-    public String getHello() {
-        return infoCreator.sayHello();
+    public void setFirstName(String firstName) {
+        infoCreator.setFirstName(firstName);
+    }
+    
+    public void setLastName(String lastName) {
+        infoCreator.setLastName(lastName);
+        createAccount();
+    }
+    
+    public void createAccount() {
+        String createAccount = infoCreator.createAccount();
+        log.info("-->> " + createAccount);
     }
 }
+
