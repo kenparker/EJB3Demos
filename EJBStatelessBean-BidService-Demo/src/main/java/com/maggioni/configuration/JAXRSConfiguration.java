7acc5defc1ee86b561415e9d7456fc0f56195b0e
magang <magang@magang-PC.fritz.box>
2017-03-30 16:08:24 +0200

    BidService moved to Sample-Demos Project / EJB3Demos

--- EJBStatelessBean-BidService-Demo/src/main/java/com/maggioni/configuration/JAXRSConfiguration.java
+++
@@ -1,9 +0,0 @@
-package com.maggioni.configuration;
-
-import javax.ws.rs.ApplicationPath;
-import javax.ws.rs.core.Application;
-
-@ApplicationPath("resources")
-public class JAXRSConfiguration extends Application {
-
-}
