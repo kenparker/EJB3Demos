4232c42cfc681ad346b050860a83700329c47b4c
magang <magang@magang-PC.fritz.box>
2017-03-30 16:04:49 +0200

    EJB-StatelessBean-LibrarySessionBean moved to Sample-Demos Project / EJB3 Demos

--- EJBStatelessBean-LibrarySessionBean-Demo/src/main/java/com/maggioni/service/JAXRSConfiguration.java
+++
@@ -1,9 +0,0 @@
-package com.maggioni.service;
-
-import javax.ws.rs.ApplicationPath;
-import javax.ws.rs.core.Application;
-
-@ApplicationPath("resources")
-public class JAXRSConfiguration extends Application {
-
-}
