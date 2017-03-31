3562abd77e0546217d1ae066ae8abb41a8192782
angelo <magangde@yahoo.de>
2017-03-31 11:01:45 +0200

    moved to subfolder

---
+++ Maven-Basic-Projects/Maven-Basics-Demo-micro-WildFly-10-1/Maven-Basic-micro-WildFly-10-1-EJB/src/main/java/com/maggioni/maven/basic/ejb/BasicEBJ.java
@@ -0,0 +1,14 @@
+package com.maggioni.maven.basic.ejb;
+
+import java.util.logging.Logger;
+import javax.ejb.Stateless;
+
+@Stateless
+public class BasicEBJ {
+    
+    private static final Logger log = Logger.getLogger("root.BasicEBJ");
+    
+    public void sayHello() {
+        log.info("-->> say Hello");
+    }
+}
