d662d63827e21be5c0c12f26a7def03c76fb1df2
angelo <magangde@yahoo.de>
2017-03-31 11:02:24 +0200

    moved to subfolder

---
+++ Maven-Basic-Projects/Maven-Basics-Demo/Maven-Basic-WEB/src/main/java/com/maggioni/boundary/BasicServlet.java
@@ -0,0 +1,50 @@
+package com.maggioni.boundary;
+
+import com.maggioni.maven.basic.ejb.BasicEBJ;
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.ejb.EJB;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+@WebServlet(name = "BasicServlet", urlPatterns = {"/BasicServlet"})
+public class BasicServlet extends HttpServlet {
+
+    @EJB
+    BasicEBJ basicEBJ;
+    
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+            /* TODO output your page here. You may use following sample code. */
+            out.println("<!DOCTYPE html>");
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title>Servlet BasicServlet</title>");            
+            out.println("</head>");
+            out.println("<body>");
+            out.println("<h1>Servlet BasicServlet at " + request.getContextPath() + "</h1>");
+            basicEBJ.sayHello();
+            out.println("</body>");
+            out.println("</html>");
+        }
+    }
+
+    
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    
+}
