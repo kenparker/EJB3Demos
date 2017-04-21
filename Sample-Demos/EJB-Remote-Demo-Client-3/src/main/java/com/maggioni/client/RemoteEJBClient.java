e3cc5687db2f7e378dbf7b4fbdd7279d222fe686
magang <magang@magang-PC.fritz.box>
2017-04-21 10:46:40 +0200

    Error:EJB not found

--- Sample-Demos/EJB-Remote-Demo-Client-3/src/main/java/com/maggioni/client/RemoteEJBClient.java
+++ Sample-Demos/EJB-Remote-Demo-Client-3/src/main/java/com/maggioni/client/RemoteEJBClient.java
@@ -80,31 +80,14 @@ public class RemoteEJBClient {
      * @throws NamingException
      */
     private static RemoteCalculator lookupRemoteStatelessCalculator() throws NamingException {
-        final Hashtable<String, String> jndiProperties = new Hashtable<>();
+        final Hashtable jndiProperties = new Hashtable<>();
         jndiProperties.put(Context.URL_PKG_PREFIXES, "org.jboss.ejb.client.naming");
+        jndiProperties.put("jboss.naming.client.ejb.context", true);
         final Context context = new InitialContext(jndiProperties);
 
-        // The JNDI lookup name for a stateless session bean has the syntax of:
-        // ejb:<appName>/<moduleName>/<distinctName>/<beanName>!<viewClassName>
-        //
-        // <appName> The application name is the name of the EAR that the EJB is deployed in
-        // (without the .ear). If the EJB JAR is not deployed in an EAR then this is
-        // blank. The app name can also be specified in the EAR's application.xml
-        //
-        // <moduleName> By the default the module name is the name of the EJB JAR file (without the
-        // .jar suffix). The module name might be overridden in the ejb-jar.xml
-        //
-        // <distinctName> : WildFly allows each deployment to have an (optional) distinct name.
-        // This example does not use this so leave it blank.
-        //
-        // <beanName> : The name of the session been to be invoked.
-        //
-        // <viewClassName>: The fully qualified classname of the remote interface. Must include
-        // the whole package name.
-
+        // java:jboss/exported/Remote/EJB-Remote-Demo-ejb-1.0/CalculatorBean!com.maggioni.Stateless2.RemoteCalculator
         // let's do the lookup
-        final String jndiname = "ejb:/Remote/Wildfly-Quickstart-EJB-Remote-Demo-1-ejb/CalculatorBean!"
-                + RemoteCalculator.class.getName();
+        final String jndiname = "ejb:Remote/EJB-Remote-Demo-ejb-1.0//CalculatorBean!" + RemoteCalculator.class.getName();
         System.out.println("jndiname is : " + jndiname);
         return (RemoteCalculator) context.lookup(jndiname);
     }
