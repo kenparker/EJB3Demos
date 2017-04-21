32cac00f76f14cf021433458a4ba8c8a78c6e70f
magang <magang@magang-PC.fritz.box>
2017-04-21 10:51:15 +0200

    Error:EJB not found / minor changes

--- Sample-Demos/EJB-Remote-Demo-Client-3/src/main/java/com/maggioni/client/RemoteEJBClient.java
+++ Sample-Demos/EJB-Remote-Demo-Client-3/src/main/java/com/maggioni/client/RemoteEJBClient.java
@@ -1,19 +1,3 @@
-/*
- * JBoss, Home of Professional Open Source
- * Copyright 2015, Red Hat, Inc. and/or its affiliates, and individual
- * contributors by the @authors tag. See the copyright.txt in the
- * distribution for a full listing of individual contributors.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- * http://www.apache.org/licenses/LICENSE-2.0
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 package com.maggioni.client;
 
 import com.maggioni.Stateless2.RemoteCalculator;
@@ -23,14 +7,6 @@ import javax.naming.NamingException;
 
 import java.util.Hashtable;
 
-
-
-/**
- * A sample program which acts a remote client for a EJB deployed on AS7 server. This program shows how to lookup stateful and
- * stateless beans via JNDI and then invoke on them
- *
- * @author Jaikiran Pai
- */
 public class RemoteEJBClient {
 
     public static void main(String[] args) throws Exception {
@@ -39,11 +15,6 @@ public class RemoteEJBClient {
 
     }
 
-    /**
-     * Looks up a stateless bean and invokes on it
-     *
-     * @throws NamingException
-     */
     private static void invokeStatelessBean() throws NamingException {
         // Let's lookup the remote stateless calculator
         final RemoteCalculator statelessRemoteCalculator = lookupRemoteStatelessCalculator();
@@ -71,14 +42,6 @@ public class RemoteEJBClient {
         }
     }
 
-    
-
-    /**
-     * Looks up and returns the proxy to remote stateless calculator bean
-     *
-     * @return
-     * @throws NamingException
-     */
     private static RemoteCalculator lookupRemoteStatelessCalculator() throws NamingException {
         final Hashtable jndiProperties = new Hashtable<>();
         jndiProperties.put(Context.URL_PKG_PREFIXES, "org.jboss.ejb.client.naming");
