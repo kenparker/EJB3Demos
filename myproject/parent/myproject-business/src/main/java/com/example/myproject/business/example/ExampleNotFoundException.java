cb43adef08f8511982c3a3debf7078b5d43d494b
angelo <magangde@yahoo.de>
2017-04-24 16:53:47 +0200

    myProject removed

--- myproject/parent/myproject-business/src/main/java/com/example/myproject/business/example/ExampleNotFoundException.java
+++
@@ -1,9 +0,0 @@
-package com.example.myproject.business.example;
-
-public class ExampleNotFoundException extends Exception {
-
-    ExampleNotFoundException(final String message) {
-        super(message);
-    }
-    
-}
